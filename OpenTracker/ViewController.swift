f7846f7ac6daa443af9a96b139c9243533e1dec0
Alexander Skorulis <skorulis@gmail.com>
2018-10-11 07:35:05 +1100

    Initial Commit

---
+++ OpenTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenTracker
+//
+//  Created by Alexander Skorulis on 11/10/18.
+//  Copyright © 2018 Alexander Skorulis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
